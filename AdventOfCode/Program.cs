cfe9a3f744a637a52e49b3f2209a3c0b6b3b3c41
Edmund Woodfield <edmundwoodfield@gmail.com>
2023-12-06 14:43:22 +0000

    path changed to relative

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-string path = @"C:\Users\edmun\Documents\AdventOfCode\AdventOfCode\day1input.txt";
+string path = @"..\..\..\day1input.txt";
 string[] lines;
 lines = File.ReadAllLines(path);
 Numbers.CalculateTotal(lines);
