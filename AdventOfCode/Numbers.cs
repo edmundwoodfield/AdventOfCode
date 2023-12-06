c28912f070ca662e4034b2fd4325bf61602a6e07
Edmund Woodfield <edmundwoodfield@gmail.com>
2023-12-06 13:41:59 +0000

    day 1 part 2 solved

--- AdventOfCode/Numbers.cs
+++ AdventOfCode/Numbers.cs
@@ -4,22 +4,91 @@ double total = 0;
 List<String> numerals = new List<string>();
 foreach (string line in lines){
             string lineDigits = "";
+            string lineLetters = "";
     foreach (char character in line){
-        if(char.IsDigit(character)){
-            lineDigits = string.Concat(lineDigits,character);
-        }
+                if (char.IsDigit(character)){
+                    CheckLineLetters(ref lineDigits,ref lineLetters);
+                    lineLetters = "";
+                    lineDigits = string.Concat(lineDigits,character);
+                     }
+                else {
+                    lineLetters = string.Concat(lineLetters, character);
+            }
+    CheckLineLetters(ref lineDigits,ref lineLetters);
     }
-    numerals.Add(lineDigits);
-}
-foreach(string numeral in numerals){
+numerals.Add(lineDigits);
+    }
+    foreach(string numeral in numerals){
     char left = numeral[0];
     char right = numeral[numeral.Length-1];
     double tens = char.GetNumericValue(left)*10;
     double units = char.GetNumericValue(right);
     total += tens + units;
-}
+    }
 Console.WriteLine("the total is " + total);
 return total;
 }
 
+    private static void CheckLineLetters(ref string lineDigits, ref string lineLetters)
+    {
+        //this currently repeats the text numeral until the next numeral is found or a later text numeral is found
+        //this does not affect the result because the first and last digits are correct
+        if (lineLetters.Contains("one"))
+        {
+            int pos = lineLetters.IndexOf("one");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("one", "");
+            lineDigits = string.Concat(lineDigits, "1");
+        }
+        if (lineLetters.Contains("two"))
+        {int pos = lineLetters.IndexOf("two");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("two", "");
+            lineDigits = string.Concat(lineDigits, "2");
+        }
+        if (lineLetters.Contains("three"))
+        {int pos = lineLetters.IndexOf("three");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("three", "");
+            lineDigits = string.Concat(lineDigits, "3");
+        }
+        if (lineLetters.Contains("four"))
+        {int pos = lineLetters.IndexOf("four");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("four", "");
+            lineDigits = string.Concat(lineDigits, "4");
+        }
+        if (lineLetters.Contains("five"))
+        {int pos = lineLetters.IndexOf("five");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("five", "");
+            lineDigits = string.Concat(lineDigits, "5");
+        }
+        if (lineLetters.Contains("six"))
+        {int pos = lineLetters.IndexOf("six");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("six", "");
+            lineDigits = string.Concat(lineDigits, "6");
+        }
+        if (lineLetters.Contains("seven"))
+        {int pos = lineLetters.IndexOf("seven");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("seven", "");
+            lineDigits = string.Concat(lineDigits, "7");
+        }
+        if (lineLetters.Contains("eight"))
+        {int pos = lineLetters.IndexOf("eight");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("eight", "");
+            lineDigits = string.Concat(lineDigits, "8");
+        }
+        if (lineLetters.Contains("nine"))
+        {int pos = lineLetters.IndexOf("nine");
+            lineLetters = lineLetters.Remove(0,pos);
+            // lineLetters = lineLetters.Replace("nine", "");
+            lineDigits = string.Concat(lineDigits, "9");
+        }
+    }
+
+    
 }
--- AdventOfCode/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ AdventOfCode/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57169db21735c411951ae100971e92c05d25d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c8adb25e6aa29a21e2c163c846d0a56657dcc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
